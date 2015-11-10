0b248b3216e67781bfb167ef5b93a52eb12b508f
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-11-09 23:40:35 -0300

    Finish "Build static version in React - part 1" lesson

---
+++ THINKING IN REACT/solution.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const FilterableProductTable = React.createClass({
+    render () {
+        return (
+            <div></div>
+        );
+    }
+});
+
+module.exports.FilterableProductTable = FilterableProductTable;
