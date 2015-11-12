1d4fd547d86cf5bb4ced40cdeb6e9269d646ae19
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-11-11 21:38:53 -0300

    Finish "Step 2 : Build static version in React - part 2" lesson

--- THINKING IN REACT/solution.js
+++ THINKING IN REACT/solution.js
@@ -3,9 +3,23 @@ import React from 'react';
 export const FilterableProductTable = React.createClass({
     render () {
         return (
-            <div></div>
+            <SearchBar/>
+        );
+    }
+});
+
+export const SearchBar = React.createClass({
+    render () {
+        return (
+            <form>
+                <input type="text" placeholder="Search..."/>
+                <label>
+                    <input type="checkbox"/> Only show products in stock
+                </label>
+            </form>
         );
     }
 });
 
 module.exports.FilterableProductTable = FilterableProductTable;
+module.exports.SearchBar = SearchBar;
